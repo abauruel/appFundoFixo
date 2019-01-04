b561d2d409cf1bdbcaaa3335fb9694a7b2530ccf
Alex.Claude <alex.claude@cmng.com.br>
2019-01-04 16:29:49 -0200

    adicionado controllers

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,14 @@ const authMiddleware = require("./middlewares/auth");
 
 const UserController = require("./controllers/UserController");
 const SessionController = require("./controllers/SessionController");
+const DespesaController = require("./controllers/DespesaController");
+const ModalController = require("./controllers/ModalController");
+const MovimentoController = require("./controllers/MovimentoController");
 
 routes.post("/users", UserController.store);
 routes.post("/session", SessionController.store);
+routes.post("/despesa", DespesaController.store);
+routes.post("/modal", ModalController.stores);
 
 routes.get("/teste", authMiddleware, (req, res) => res.json({ ok: true }));
 
